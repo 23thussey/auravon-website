3f8d2d907f73c76847526a0edd161589933ab7fa
23thussey <tommyboy747h@gmail.com>
2024-12-02 20:33:24 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// scripts.js
+
+function openTradeForm() {
+
+    alert("Thank you for your interest in trading with Auravon. Please email trade@auravon.gov for inquiries.");
+
+}
+
+document.getElementById("citizenship-form").addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    alert("Thank you for applying! We will review your application and get back to you.");
+
+});
